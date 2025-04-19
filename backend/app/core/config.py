c5aab9211313dfcda1c0e06d4a6dc5af1fdd1c38
enzomendez315 <enzo.mendez315@gmail.com>
2025-04-19 03:22:32 -0600

    Set up db config.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,50 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+import secrets
 
+from typing import Literal
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_core import MultiHostUrl
+from pydantic import (
+    PostgresDsn,
+    HttpUrl,
+    computed_field,
+    EmailStr
+)
 
 class Settings(BaseSettings):
-    pass
+    model_config = SettingsConfigDict(
+        # Use top level .env file (one level above ./backend/)
+        env_file="../.env",
+        env_ignore_empty=True,
+        extra="ignore"
+    )
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 7 days = 7 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+    FRONTEND_HOST: str = "http://localhost:3000"
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+
+    PROJECT_NAME: str
+    SENTRY_DSN: HttpUrl | None = None
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
+
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
+    FIRST_SUPERUSER: EmailStr
+    FIRST_SUPERUSER_PASSWORD: str
+
+settings = Settings()  # type: ignore
--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,3 +1,11 @@
 from sqlmodel import Session, create_engine, select
 
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+from app.core.config import settings
+from app.models import User
+
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
+def init_db(session: Session) -> None:
+    user = session.exec(
+        select(User).where(User.email == settings.FIRST_SUPERUSER)
+    ).first()
